f32d2431fd7e567ddee1f938e4853a21a2125369
Artem Mitrofanov <artem.bvdf@mail.ru>
2024-10-26 20:57:34 +0500

    Header ver 0.1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Search from "./components/Search/Search.jsx";
+import Header from "./components/Header/Header.jsx";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Search />
     </div>
   );
