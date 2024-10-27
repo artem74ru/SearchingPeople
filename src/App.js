8e1f441046e33f47b59f602bf833635748e85401
Artem Mitrofanov <artem.bvdf@mail.ru>
2024-10-27 21:00:54 +0500

    Tabs ver 0.1 + fetchUsers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Search from "./components/Search/Search.jsx";
 import Header from "./components/Header/Header.jsx";
+import Tab   from "./components/Tab/Tab.jsx";
 import './App.css';
+import axios from "axios";
+
+async function fetchUsers(params) {
+  const response = await axios.get('https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users')
+  console.log(response.data.items)
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <Tab />
+      <button onClick={fetchUsers}>Get Users</button>
+    
+
     </div>
   );
 }
---
+++ src/components/Tab/Tab.jsx
@@ -0,0 +1,41 @@
+import "./Tab.css";
+
+export default function Tab() {
+  return (
+    <div className="tab-wrapper">
+      <div className="tab">
+        <input id="tab__btn-1" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-1">Все</label>
+        <input id="tab__btn-2" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-2">Designers</label>
+        <input id="tab__btn-3" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-3">Analysts</label>
+        <input id="tab__btn-4" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-4">Managers</label>
+        <input id="tab__btn-5" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-5">iOS</label>
+        <input id="tab__btn-6" name="tab__btn" type="radio" value="" />
+        <label for="tab__btn-6">Android</label>
+
+        <div class="tab-content" id="content-1">
+          Все
+        </div>
+        <div class="tab-content" id="content-2">
+          Дизайнеры
+        </div>
+        <div class="tab-content" id="content-3">
+          Аналитики
+        </div>
+        <div class="tab-content" id="content-4">
+          Менеджеры
+        </div>
+        <div class="tab-content" id="content-5">
+          IOS
+        </div>
+        <div class="tab-content" id="content-6">
+          Android
+        </div>
+      </div>
+    </div>
+  );
+}
