6a567b4bbc609847ac774c0144de630b05ced4da
user_Artem <artem.bvdf@mail.ru>
2024-10-28 16:15:41 +0500

    api all

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import './App.css';
 import axios from "axios";
 
 const options = {
-  method: "GET",
+  method: "GET",  
   url: "https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users",
-  params: { __example: "ios" }, // получение из разных департаментов
+  params: { __example: "all" }, // получение из разных департаментов
   headers: { Accept: "application/json, application/xml" },
 };
 
 async function Users() {
     try {
     const { data } = await axios.request(options);
-    console.log(data);
+    console.log(data.items[0].firstName);
+    console.log(data.items[1].firstName);
+    console.log(data.items[2].firstName);
+    console.log(data.items[3].firstName);
+    console.log(data.items[4].firstName);
+    console.log(data.items[5].firstName);
+    console.log(data.items[6].firstName);
+    console.log(data.items[7].firstName);
+    console.log(data.items[8].firstName);
+    console.log(data.items[9].firstName);
+    console.log(data.items[10].firstName);
+    console.log(data.items[11].firstName);
     } catch (error) {
       console.error(error);
   }
 }
-
 
 function App() {
   return (
@@ -28,6 +38,7 @@ function App() {
       <Search />
       <Tab />
       <button onClick={Users}>Get Users</button>
+     
     </div>
   );
 }
